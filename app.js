e0580ba04e2c994dbaad51b9d9a522cad45b8686
pou0119 <yuuyiteng0@gmail.com>
2024-06-11 09:51:37 +0900

    update app.js

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ const port = 3000;
 
 app.use(express.static('public'));
 
+app.get('/header.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'header.html'));
+});
+  
+app.get('/footer.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'footer.html'));
+});
+
 app.get('/', (req, res) => {
-    res.render('top.html');
+    res.sendFile(path.join(__dirname, 'top.html'));
 });
 
 app.listen(port, () => {
