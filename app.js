fc4da53b5cca4a5b31c149868ec78355488f302a
pou0119 <yuuyiteng0@gmail.com>
2024-06-11 13:53:56 +0900

    change html -> ejs

--- app.js
+++ app.js
@@ -3,25 +3,46 @@ const path=require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.get('/header.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'header.html'));
+app.use(express.static('public'));
+app.use(express.static('views'))
+
+const mysql = require('mysql');
+const exp = require('constants');
+
+// MySQLとの接続情報
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'SYaa0119kyukouDai0ll9',
+  database: 'kit'
+});
+
+
+app.get('/header', (req, res) => {
+    res.render('header');
 });
   
-app.get('/footer.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'footer.html'));
+app.get('/footer', (req, res) => {
+    res.render('footer');
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'top.html'));
-});
 
-app.get('/menber', (req, res) => {
-    res.sendFile(path.join(__dirname, 'members.html'));
+        // top.htmlをクライアントに送信する前に、データをテンプレートに渡して組み込む
+        res.render('top.ejs');
+    });
+
+
+
+app.get('/member', (req, res) => {
+    res.render('member');
 });
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
   
+
--- public/js/main.js
+++
@@ -1,6 +0,0 @@
-fetch("../../header.html")
-    .then((response)=>response.text())
-    .then((data)=>document.querySelector("#header").innerHTML=data);
-fetch("footer.html")
-    .then((response) => response.text())
-    .then((data) => document.querySelector("#footer").innerHTML = data);
