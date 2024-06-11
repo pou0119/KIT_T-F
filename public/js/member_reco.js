c8412e62180b7ce5c72366f0a112fbec309a3790
pou0119 <yuuyiteng0@gmail.com>
2024-06-12 02:47:06 +0900

    update member.ejs

--- public/js/member_reco.js
+++ public/js/member_reco.js
@@ -42,3 +42,29 @@ function openCard($card) {
     });
     
 }
+
+// window.onload = function() {
+//   // 新しいプロフィール写真のURL
+//   var newProfilePictureURL = "/img/KIT_logo.gif";
+  
+//   // .img-wrapper要素を取得
+//   var imgWrappers = document.querySelectorAll(".img-wrapper");
+
+//   // すべての.img-wrapper要素に対してループを実行し、背景画像を変更
+//   imgWrappers.forEach(function(imgWrapper, index) {
+//       if (index === 0) {
+//           // 一番目の.img-wrapper要素
+//           imgWrapper.style.backgroundImage = "url('" + newProfilePictureURL + "')";
+//       } else if (index === 1) {
+//           // 二番目の.img-wrapper要素
+//           // ここに背景画像を変更するコードを追加
+//       }
+//       // 他の要素に対する条件分岐を追加することもできます
+//   });
+// };
+
+var arrayElement = document.getElementById("array-container");
+var arrayDataFromHTML = JSON.parse(arrayElement.dataset.array);
+
+// データを表示
+console.log(arrayDataFromHTML[1]);
