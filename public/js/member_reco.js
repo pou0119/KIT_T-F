395d5509aa72570ab26be90f23abd5ca249540e0
pou0119 <yuuyiteng0@gmail.com>
2024-06-12 10:21:37 +0900

    member.ejs update

--- public/js/member_reco.js
+++ public/js/member_reco.js
@@ -43,28 +43,24 @@ function openCard($card) {
     
 }
 
-// window.onload = function() {
-//   // 新しいプロフィール写真のURL
-//   var newProfilePictureURL = "/img/KIT_logo.gif";
-  
-//   // .img-wrapper要素を取得
-//   var imgWrappers = document.querySelectorAll(".img-wrapper");
+window.onload = function() {
+    var arrayElement = document.getElementById("array-container");
+    var arrayDataFromHTML = JSON.parse(arrayElement.dataset.array);
+    
+    // データを表示
+    console.log(arrayDataFromHTML[1]);
+    
+    // .img-wrapper要素を取得
+    var imgWrappers = document.querySelectorAll(".img-wrapper");
+    const imgWrapperCount = imgWrappers.length;
+    let i = 0;
+    // すべての.img-wrapper要素に対してループを実行し、背景画像を変更
+    imgWrappers.forEach(function(imgWrapper, index) {
+        if (index === i) {
+            // 一番目の.img-wrapper要素
+            imgWrapper.style.backgroundImage = "url('" + arrayDataFromHTML[i] + "')";
+        } 
+        i++;
+    });
+};
 
-//   // すべての.img-wrapper要素に対してループを実行し、背景画像を変更
-//   imgWrappers.forEach(function(imgWrapper, index) {
-//       if (index === 0) {
-//           // 一番目の.img-wrapper要素
-//           imgWrapper.style.backgroundImage = "url('" + newProfilePictureURL + "')";
-//       } else if (index === 1) {
-//           // 二番目の.img-wrapper要素
-//           // ここに背景画像を変更するコードを追加
-//       }
-//       // 他の要素に対する条件分岐を追加することもできます
-//   });
-// };
-
-var arrayElement = document.getElementById("array-container");
-var arrayDataFromHTML = JSON.parse(arrayElement.dataset.array);
-
-// データを表示
-console.log(arrayDataFromHTML[1]);
